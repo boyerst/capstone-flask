69e02d39c75ab9dcc8fef0f8ebb90545dd8f60f1
Steve Boyer <stephen.a.boyer@gmail.com>
2020-05-10 15:41:34 -0500

    user can login and logout

--- resources/users.py
+++ resources/users.py
@@ -1,8 +1,8 @@
 import models 
-
 from flask import Blueprint, request, jsonify
 from flask_bcrypt import generate_password_hash, check_password_hash
 from flask_login import login_user, current_user, logout_user
+from playhouse.shortcuts import model_to_dict
 
 users = Blueprint('users', 'users')
 
@@ -48,11 +48,51 @@ def register():
       status=201
     ), 201
 
+
+
 #LOGIN /users/login
 @users.route('/login', methods=['POST'])
 def login():
-  return "login route hitting"
+  payload = request.get_json()
+  payload['email'] = payload['email'].lower()
+  payload['username'] = payload['username'].lower()
+  try: 
+    user = models.User.get(models.User.email == payload['email'])
+    user_dict = model_to_dict(user)
+    password_is_good = check_password_hash(user_dict['password'], payload['password'])
+    if(password_is_good):
+      login_user(user)
+      user_dict.pop('password')
+      return jsonify(
+        data=user_dict,
+        message=f"Successfully logged in {user_dict['email']}",
+        status=200
+      ), 200
+    else: 
+      print('pw is no good')
+      return jsonify(
+        data={},
+        message="Email or password is incorrect", 
+        status=401
+      ), 401
+  except models.DoesNotExist: 
+    print('The username is not correct')
+    return jsonify(
+      data={},
+      message="The email or password is incorrect", 
+      status=401
+    ), 401
 
+
+#LOGOUT /users/logout
+@users.route('/logout', methods=['GET'])
+def logout():
+  logout_user()
+  return jsonify(
+    data={}, 
+    message="Successfully logged out",
+    status=200
+  ), 200
 
 
 
