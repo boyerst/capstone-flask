7dd0a125cf7a26ee1c6c5df14b796cccf90a77bf
Steve Boyer <stephen.a.boyer@gmail.com>
2020-05-10 14:59:40 -0500

    login route is hitting

--- resources/users.py
+++ resources/users.py
@@ -18,16 +18,16 @@ def test():
 #REGISTER /users/register
 @users.route('/register', methods=['POST'])
 def register():
-  print(request.get_json())
   payload = request.get_json()
+  print(request.get_json())
+  print(payload) 
   payload['email'] = payload['email'].lower()
   payload['username'] = payload['username'].lower()
-  print(payload) 
   try:
     models.User.get(models.User.email == payload['email'])
     return jsonify(
       data={},
-      message=f"user with the email {payload['email']} already exists",
+      message=f"A user with the email {payload['email']} already exists",
       status=401
     ), 401
   except models.DoesNotExist:
@@ -37,8 +37,32 @@ def register():
       password=generate_password_hash(payload['password'])
     )
     print(created_user)
+    login_user(created_user) 
+    created_user_dict = model_to_dict(created_user)
+    print(created_user_dict)
+    print(type(created_user_dict['password']))
+    created_user_dict.pop('password')
     return jsonify(
       data=created_user_dict,
-      message=f"Successfully registered user {created_user_dict['email']}",
+      message=f"You have successfully registered user {created_user_dict['email']}",
       status=201
     ), 201
+
+#LOGIN /users/login
+@users.route('/login', methods=['POST'])
+def login():
+  return "login route hitting"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
