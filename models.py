743ee6415dc61b83daa4151e972ccea64840c168
Steve Boyer <steveboyer@Macbook-2.local>
2020-05-09 22:30:54 -0500

    installed peewee, psycopg2, db created, add routes resource, blueprint

---
+++ models.py
@@ -0,0 +1,41 @@
+
+from peewee import *
+import datetime 
+
+
+
+DATABASE = SqliteDatabase('routes.sqlite') 
+
+
+
+
+
+class Route(Model):
+  rider_id = CharField()
+  route_id = CharField()
+  location = CharField() 
+  length = IntegerField()
+  skill_level = IntegerField()
+  obstacles = CharField()
+  comments = CharField()
+  created_at: DateTimeField(default=datetime.datetime.now)
+  
+
+
+  class Meta: 
+    database = DATABASE 
+
+
+
+
+def initialize(): 
+  DATABASE.connect() 
+
+
+  DATABASE.create_tables([Route], safe=True)
+  print("Connected to DB and created tables if they weren't already there")
+
+  DATABASE.close()
+
+
+
