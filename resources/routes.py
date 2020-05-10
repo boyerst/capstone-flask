df7aede8c9128c7ceda39c6e69d56326027a58b5
Steve Boyer <steveboyer@Macbook-2.local>
2020-05-09 22:46:18 -0500

    create route in db

--- resources/routes.py
+++ resources/routes.py
@@ -1,12 +1,34 @@
 import models
 
 
-from flask import Blueprint
+from flask import Blueprint, request
 
 
 
 routes = Blueprint('routes', 'routes')
 
+
+
+
+
+
+
 @routes.route('/')
 def routes_index():
   return "routes resource working" 
+
+
+@routes.route('/', methods=['POST'])
+def create_route():
+  payload = request.get_json()
+  print(payload)
+  new_route = models.Route.create(
+    rider_id=payload['rider_id'],
+    route_id=payload['route_id'],
+    location=payload['location'], 
+    length=payload['length'], 
+    skill_level=payload['skill_level'],
+    obstacles=payload['obstacles'], 
+    comments=payload['comments'])
+  print(new_route)
+  return "route create route hitting - check terminal"
