7f19f37c004abeb35cdad7bb931ee82d243c664a
Steve Boyer <stephen.a.boyer@gmail.com>
2020-05-11 14:30:58 -0500

    changed/renamed model variables in models and all current routes

--- resources/markers.py
+++ resources/markers.py
@@ -5,6 +5,7 @@ from flask import Blueprint, request, jsonify
 
 from playhouse.shortcuts import model_to_dict 
 
+from flask_login import current_user, login_required
 
 markers = Blueprint('markers', 'markers')
 
@@ -13,7 +14,7 @@ markers = Blueprint('markers', 'markers')
 
 #INDEX /markers
 @markers.route('/', methods=['GET'])
-def routes_index():
+def markers_index():
   result = models.Marker.select()
   marker_dicts = [model_to_dict(marker) for marker in result]
 
@@ -32,9 +33,11 @@ def create_marker():
   payload = request.get_json()
   print(payload)
   new_marker = models.Marker.create(
-    marker_id=payload['marker_id'],
+    route_id=payload['route_id'],
     latitude=payload['latitude'],
-    longitude=payload['longitude']
+    longitude=payload['longitude'],
+    image=payload['image'],
+    description=payload['description']
     )
   marker_dict=model_to_dict(new_marker)
   print(new_marker)
