b7ec2f15a6994aa07af5dca7647aa3467b4b769f
Steve Boyer <steveboyer@Macbook-2.local>
2020-06-22 19:35:49 -0500

    heroku config

--- app.py
+++ app.py
@@ -38,25 +38,25 @@ print(app.secret_key)
 
 
 # SESSION
-@login_manager.user_loader
-def load_user(user_id):
-  try:
-    print("loading the following user")
-    user = models.User.get_by_id(user_id) 
-    return user 
-  except models.DoesNotExist: 
-    return None
+# @login_manager.user_loader
+# def load_user(user_id):
+#   try:
+#     print("loading the following user")
+#     user = models.User.get_by_id(user_id) 
+#     return user 
+#   except models.DoesNotExist: 
+#     return None
 
-# AUTH
-@login_manager.unauthorized_handler
-def unauthorized():
-  return jsonify(
-    data={
-      'error': "User not logged in"
-    },
-    message='You must be logged in to access that resource',
-    status=401
-  ), 401
+# # AUTH
+# @login_manager.unauthorized_handler
+# def unauthorized():
+#   return jsonify(
+#     data={
+#       'error': "User not logged in"
+#     },
+#     message='You must be logged in to access that resource',
+#     status=401
+#   ), 401
 
 
 
