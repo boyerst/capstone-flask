34cb27b5468d68df8e753ba9304acd7554be764c
Steve Boyer <stephen.a.boyer@gmail.com>
2021-02-25 18:29:03 -0600

    reverted secret key

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ app = Flask(__name__)
 
 # app = Flask(__name__, static_folder="./static/dist", template_folder="./static")
 
-app.secret_key = "New Secret Key"
+app.secret_key = "Secret Time"
 login_manager = LoginManager()
 login_manager.init_app(app)
 
