593d56b8c6287aa92b5d94fce9eb04f09a7e0fdc
Steve Boyer <stephen.a.boyer@gmail.com>
2021-03-01 11:26:13 -0600

    user_id to id

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ print(app.secret_key)
 def load_user(id):
   try:
     print("loading the following user")
-    return models.User.get_by_id(user_id) 
+    return models.User.get_by_id(id) 
   except models.DoesNotExist: 
     return None
 
