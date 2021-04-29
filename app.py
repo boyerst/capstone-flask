a27541fd6de40d020392e86d546e4ebd7cdabd93
Steve Boyer <stephen.a.boyer@gmail.com>
2021-04-28 20:43:34 -0500

    trying new user_loader

--- app.py
+++ app.py
@@ -1,39 +1,39 @@
 
-# from flask import Flask, g, jsonify
-# import os
-# from resources.users import users
-# from resources.routes import routes
-# from resources.markers import markers
-# # from flask import make_response, Response
+from flask import Flask, g, jsonify
+import os
+from resources.users import users
+from resources.routes import routes
+from resources.markers import markers
+# from flask import make_response, Response
 
-# import models
-# from flask_cors import CORS
+import models
+from flask_cors import CORS
 
-# from flask_login import LoginManager
-# # import logging
-# # import json, commands, requests, sys
+from flask_login import LoginManager
+# import logging
+# import json, commands, requests, sys
 
-# DEBUG=True 
-# PORT=8000 
+DEBUG=True 
+PORT=8000 
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# # app = Flask(__name__, static_folder="./static/dist", template_folder="./static")
+# app = Flask(__name__, static_folder="./static/dist", template_folder="./static")
 
-# app.secret_key = "secret time"
-# login_manager = LoginManager()
-# login_manager.init_app(app)
+app.secret_key = "secret time"
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-# # app.logger.addHandler(logging.StreamHandler(sys.stdout))
-# # app.logger.setLevel(logging.ERROR)
-
-
-# print("Here is the app secret_key:")
-# print(app.secret_key)
+# app.logger.addHandler(logging.StreamHandler(sys.stdout))
+# app.logger.setLevel(logging.ERROR)
 
 
+print("Here is the app secret_key:")
+print(app.secret_key)
 
-# # SESSION
+
+
+# SESSION
 # @login_manager.user_loader
 # def load_user(user_id):
 #   try:
@@ -42,162 +42,52 @@
 #   except models.DoesNotExist: 
 #     return None
 
-  
-
-# # @login_manager.user_loader
-# # def load_user(user_id):
-# #   try:
-# #     print("loading the following user")
-# #     user = models.User.get_by_id(user_id) 
-# #     return user 
-# #   except models.DoesNotExist: 
-# #     return None
-
-
-
-# # AUTH
-# @login_manager.unauthorized_handler
-# def unauthorized():
-#   return jsonify(
-#     data={
-#       'error': "User not logged in"
-#     },
-#     message="You must be logged in to access that resource",
-#     status=401
-#   ), 401
-
-
-# # @app.errorhandler(401)
-# # def custom_401(error):
-# #     return Response('<Why access is denied string goes here...>', 401, {'WWW-Authenticate':'Basic realm="Login Required"'})
-
-
-
-# CORS(users, origins=['http://localhost:3000', 'https://wmattracks.herokuapp.com'], supports_credentials=True)
-# CORS(routes, origins=['http://localhost:3000', 'https://wmattracks.herokuapp.com'], supports_credentials=True)
-# CORS(markers, origins=['http://localhost:3000', 'https://wmattracks.herokuapp.com'], supports_credentials=True)
-
-
-
-# app.register_blueprint(users, url_prefix='/api/v1/users')
-# app.register_blueprint(routes, url_prefix='/api/v1/routes')
-# app.register_blueprint(markers, url_prefix='/api/v1/markers')
-
-
-# @app.before_request 
-# def before_request():
-#   print("you should see this before each request") 
-#   g.db = models.DATABASE
-#   g.db.connect()
-
-# @app.after_request 
-# def after_request(response):
-#   print("you should see this after each request") #
-#   g.db.close()
-#   return response 
-       
-
-
-# @app.route('/')
-# def test():
-#   return 'test'
-
-# @app.route('/test_json')
-# def get_json():
-#   return jsonify(['jsonify', 'working'])
-
-
-# if 'ON_HEROKU' in os.environ: 
-#   print('\non heroku!')
-#   models.initialize()
-
-
-# if __name__ == '__main__':
-#   models.initialize()
-#   app.run(debug=DEBUG, port=PORT) 
-
-
-
-
-
-##################################################################
-
-
-
-
-import os
-from flask import Flask, jsonify, g
-
-from resources.routes import routes
-from resources.users import users
-from resources.markers import markers
-
-# import logging
-# import json, commands, requests, sys
-
-import models
-
-from flask_cors import CORS
-
-
-from flask_login import LoginManager
-
-DEBUG=True 
-PORT=8000 
-
-app = Flask(__name__)
-
-# app.logger.addHandler(logging.StreamHandler(sys.stdout))
-# app.logger.setLevel(logging.ERROR)
-
-# app = Flask(__name__, static_folder="./static/dist", template_folder="./static")
-
-app.secret_key = "Secret Time."
-
-login_manager = LoginManager()
-
-login_manager.init_app(app)
-
-print("Here is the app secret_key:")
-print(app.secret_key)
-
-
-
-# SESSION
 @login_manager.user_loader
 def load_user(user_id):
   try:
     print("loading the following user")
-    user = models.User.get_by_id(user_id) 
-    return user 
+    return models.User.get(user_id) 
   except models.DoesNotExist: 
     return None
 
+# @login_manager.user_loader
+# def load_user(user_id):
+#   try:
+#     print("loading the following user")
+#     user = models.User.get_by_id(user_id) 
+#     return user 
+#   except models.DoesNotExist: 
+#     return None
+
+
+
 # AUTH
-# @login_manager.unauthorized_handler
-# def unauthorized():
-#   return jsonify(
-#     data={
-#       'error': "User not logged in"
-#     },
-#     message='You must be logged in to access that resource',
-#     status=401
-#   ), 401
+@login_manager.unauthorized_handler
+def unauthorized():
+  return jsonify(
+    data={
+      'error': "User not logged in"
+    },
+    message="You must be logged in to access that resource",
+    status=401
+  ), 401
+
+
+# @app.errorhandler(401)
+# def custom_401(error):
+#     return Response('<Why access is denied string goes here...>', 401, {'WWW-Authenticate':'Basic realm="Login Required"'})
 
 
 
+CORS(users, origins=['http://localhost:3000', 'https://wmattracks.herokuapp.com'], supports_credentials=True)
+CORS(routes, origins=['http://localhost:3000', 'https://wmattracks.herokuapp.com'], supports_credentials=True)
+CORS(markers, origins=['http://localhost:3000', 'https://wmattracks.herokuapp.com'], supports_credentials=True)
 
 
 
-CORS(routes, origins=['http://localhost:3000', 'https://wmat-tracks.herokuapp.com'], supports_credentials=True)
-CORS(markers, origins=['http://localhost:3000','https://wmat-tracks.herokuapp.com'], supports_credentials=True)
-CORS(users, origins=['http://localhost:3000','https://wmat-tracks.herokuapp.com'], supports_credentials=True)
-
-
+app.register_blueprint(users, url_prefix='/api/v1/users')
 app.register_blueprint(routes, url_prefix='/api/v1/routes')
 app.register_blueprint(markers, url_prefix='/api/v1/markers')
-app.register_blueprint(users, url_prefix='/api/v1/users')
-
 
 
 @app.before_request 
@@ -212,7 +102,6 @@ def after_request(response):
   g.db.close()
   return response 
        
-
 
 
 @app.route('/')
@@ -232,3 +121,120 @@ if 'ON_HEROKU' in os.environ:
 if __name__ == '__main__':
   models.initialize()
   app.run(debug=DEBUG, port=PORT) 
+
+
+
+
+
+##################################################################
+
+
+
+
+# import os
+# from flask import Flask, jsonify, g
+
+# from resources.routes import routes
+# from resources.users import users
+# from resources.markers import markers
+
+# # import logging
+# # import json, commands, requests, sys
+
+# import models
+
+# from flask_cors import CORS
+
+
+# from flask_login import LoginManager
+
+# DEBUG=True 
+# PORT=8000 
+
+# app = Flask(__name__)
+
+# # app.logger.addHandler(logging.StreamHandler(sys.stdout))
+# # app.logger.setLevel(logging.ERROR)
+
+# # app = Flask(__name__, static_folder="./static/dist", template_folder="./static")
+
+# app.secret_key = "Secret Time."
+
+# login_manager = LoginManager()
+
+# login_manager.init_app(app)
+
+# print("Here is the app secret_key:")
+# print(app.secret_key)
+
+
+
+# # SESSION
+# @login_manager.user_loader
+# def load_user(user_id):
+#   try:
+#     print("loading the following user")
+#     user = models.User.get_by_id(user_id) 
+#     return user 
+#   except models.DoesNotExist: 
+#     return None
+
+# # AUTH
+# # @login_manager.unauthorized_handler
+# # def unauthorized():
+# #   return jsonify(
+# #     data={
+# #       'error': "User not logged in"
+# #     },
+# #     message='You must be logged in to access that resource',
+# #     status=401
+# #   ), 401
+
+
+
+
+
+
+# CORS(routes, origins=['http://localhost:3000', 'https://wmat-tracks.herokuapp.com'], supports_credentials=True)
+# CORS(markers, origins=['http://localhost:3000','https://wmat-tracks.herokuapp.com'], supports_credentials=True)
+# CORS(users, origins=['http://localhost:3000','https://wmat-tracks.herokuapp.com'], supports_credentials=True)
+
+
+# app.register_blueprint(routes, url_prefix='/api/v1/routes')
+# app.register_blueprint(markers, url_prefix='/api/v1/markers')
+# app.register_blueprint(users, url_prefix='/api/v1/users')
+
+
+
+# @app.before_request 
+# def before_request():
+#   print("you should see this before each request") 
+#   g.db = models.DATABASE
+#   g.db.connect()
+
+# @app.after_request 
+# def after_request(response):
+#   print("you should see this after each request") #
+#   g.db.close()
+#   return response 
+       
+
+
+
+# @app.route('/')
+# def test():
+#   return 'test'
+
+# @app.route('/test_json')
+# def get_json():
+#   return jsonify(['jsonify', 'working'])
+
+
+# if 'ON_HEROKU' in os.environ: 
+#   print('\non heroku!')
+#   models.initialize()
+
+
+# if __name__ == '__main__':
+#   models.initialize()
+#   app.run(debug=DEBUG, port=PORT) 
